adad4ef6d3e55a39685fe098f67a405a4009e1e6
Romans Kuznecovs <romansk@apadmi.com>
2023-12-05 11:56:16 +0000

    Remove previous implementation of quiz generation (including selectors)

--- src/main/kotlin/geotrainer/utils/QuizGenerator.kt
+++ src/main/kotlin/geotrainer/utils/QuizGenerator.kt
@@ -1,23 +1,42 @@
 package geotrainer.utils
 
 import geotrainer.models.Continent
-import geotrainer.models.countries.Country
 import geotrainer.models.quiz.QuizQuestion
 import geotrainer.utils.questionfactory.CapitalCitiesQuestionFactory
 import geotrainer.utils.questionfactory.DomainNameQuestionFactory
 import geotrainer.utils.questionfactory.DrivingSideQuestionFactory
 import geotrainer.utils.questionfactory.EuropeanUnionQuestionFactory
 import geotrainer.utils.questionfactory.QuestionFactory
-import geotrainer.utils.quizselector.QuestionAnswerSelector
 
 
 interface QuizGenerator {
+    fun generateCapitalCitiesQuiz(
+        continent: Continent?,
+        numOfQuestions: Int,
+        numOfOptions: Int,
+    ): List<QuizQuestion>
 
+    fun generateDomainNamesQuiz(
+        continent: Continent?,
+        numOfQuestions: Int,
+        numOfOptions: Int,
+    ): List<QuizQuestion>
+
+    fun generateDrivingSideQuiz(
+        continent: Continent?,
+        numOfQuestions: Int,
+        numOfOptions: Int,
+    ): List<QuizQuestion>
+
+    fun generateEuropeanUnionQuiz(
+        numOfQuestions: Int,
+        numOfOptions: Int,
+    ): List<QuizQuestion>
 }
 
 internal class QuizGeneratorImpl() : QuizGenerator {
 
-    fun generateCapitalCitiesQuiz(
+    override fun generateCapitalCitiesQuiz(
         continent: Continent?,
         numOfQuestions: Int,
         numOfOptions: Int,
@@ -26,7 +45,7 @@ internal class QuizGeneratorImpl() : QuizGenerator {
         questionFactory = CapitalCitiesQuestionFactory(numOfOptions, continent)
     )
 
-    fun generateDomainNamesQuiz(
+    override fun generateDomainNamesQuiz(
         continent: Continent?,
         numOfQuestions: Int,
         numOfOptions: Int,
@@ -35,7 +54,7 @@ internal class QuizGeneratorImpl() : QuizGenerator {
         questionFactory = DomainNameQuestionFactory(numOfOptions, continent)
     )
 
-    fun generateDrivingSideQuiz(
+    override fun generateDrivingSideQuiz(
         continent: Continent?,
         numOfQuestions: Int,
         numOfOptions: Int,
@@ -44,174 +63,13 @@ internal class QuizGeneratorImpl() : QuizGenerator {
         questionFactory = DrivingSideQuestionFactory(numOfOptions, continent)
     )
 
-    fun generateEuropeanUnionQuiz(
+    override fun generateEuropeanUnionQuiz(
         numOfQuestions: Int,
         numOfOptions: Int,
     ) = generateQuizNew(
         numOfQuestions = numOfQuestions,
         questionFactory = EuropeanUnionQuestionFactory(numOfOptions)
     )
-
-
-    /**
-     * Generates quiz where the Country type used to select the question/answer is the same Country type used
-     * to select other options that aren't the answer.
-     *
-     * */
-    @JvmName("generateQuizSameType")
-    private inline fun <reified QuestionAnswerOptionsType : Country> generateQuiz(
-        numOfQuestions: Int,
-        numOfOptions: Int,
-        continent: Continent? = null,
-        filterOptionsByContinent: Boolean = false,
-        selectors: List<QuestionAnswerSelector<QuestionAnswerOptionsType, QuestionAnswerOptionsType>>,
-    ): List<QuizQuestion> = generateQuiz<QuestionAnswerOptionsType, QuestionAnswerOptionsType>(
-        numOfQuestions = numOfQuestions,
-        numOfOptions = numOfOptions,
-        continent = continent,
-        filterOptionsByContinent = filterOptionsByContinent,
-        selectors = selectors
-    )
-
-    /**
-     * Generates quiz where the Country type used to select the question/answer is different to the Country type used
-     * to select other options that aren't the answer.
-     *
-     * The questions are always filtered by continent if not null. The options are only filtered by continent if
-     * filterOptionsByContinent = true.
-     * */
-    @JvmName("generateQuizDifferentTypes")
-    private inline fun <reified QuestionAnswerType : Country, reified OtherOptionsType : Country> generateQuiz(
-        numOfQuestions: Int,
-        numOfOptions: Int,
-        continent: Continent? = null,
-        filterOptionsByContinent: Boolean = false,
-        selectors: List<QuestionAnswerSelector<QuestionAnswerType, OtherOptionsType>>,
-    ): List<QuizQuestion> {
-        val allRelevantQuestionCountries = Country.allCountries.filterIsInstance<QuestionAnswerType>()
-            // Filter by continent
-            .let { filteredList ->
-                if (continent != null) {
-                    filteredList.filter { it.continent == continent }
-                } else {
-                    filteredList
-                }
-            }
-
-        println("DEBUG: ${allRelevantQuestionCountries.map { it::class }}")
-
-        val allRelevantOtherOptionsCountries = Country.allCountries.filterIsInstance<OtherOptionsType>()
-            // Filter by continent only if required
-            .let { filteredList ->
-                if (continent != null && filterOptionsByContinent) {
-                    filteredList.filter { it.continent == continent }
-                } else {
-                    filteredList
-                }
-            }
-
-        return generateQuiz(
-            numOfQuestions = numOfQuestions,
-            numOfOptions = numOfOptions,
-            selectors = selectors,
-            allRelevantQuestionCountries = allRelevantQuestionCountries,
-            allRelevantOtherOptionsCountries = allRelevantOtherOptionsCountries
-        )
-    }
-
-    private inline fun <reified QuestionAnswerType : Country, reified OtherOptionsType : Country> generateQuiz(
-        numOfQuestions: Int,
-        numOfOptions: Int,
-        selectors: List<QuestionAnswerSelector<QuestionAnswerType, OtherOptionsType>>,
-        // All the countries that the questions/answers/options can be selected from
-        allRelevantQuestionCountries: List<QuestionAnswerType>,
-        allRelevantOtherOptionsCountries: List<OtherOptionsType>
-    ): List<QuizQuestion> {
-        // Keeps track of countries that haven't been selected yet to make sure we don't waste a while loop cycle
-        // when we know the QuizQuestion will likely end up being rejected
-        val remainingRelevantQuestionCountries = allRelevantQuestionCountries.toMutableList()
-
-        // true = yes we care, so don't add a question that already exists
-        // false = no we don't care, so add a question that already exists
-        // This is needed for when we've run out of questions to ask and still need more questions
-        var careIfQuestionAlreadyExists = true
-        val quizQuestions = mutableListOf<QuizQuestion>()
-
-        val maxTries = numOfQuestions * 3
-        var failedTries = 0
-        while (quizQuestions.size < numOfQuestions && (failedTries <= maxTries)) {
-            val selector = selectors.random()
-            val selectedCountry = remainingRelevantQuestionCountries.random()
-
-            // Get rid of the country no matter if it's selected or not
-            // selected = remove, we don't need to select it again
-            // rejected = remove, it'll likely just get rejected again
-            remainingRelevantQuestionCountries -= selectedCountry
-
-            // Reset remaining relevant countries if we've used all countries.
-            // Also, when we reach that point then we don't need to care if questions already exist.
-            if (remainingRelevantQuestionCountries.isEmpty()) {
-                careIfQuestionAlreadyExists = false
-                remainingRelevantQuestionCountries += allRelevantQuestionCountries
-            }
-
-            val questionCategory = selector.questionCategorySelector(selectedCountry)
-            val answer = selector.answerSelector(selectedCountry)
-
-            if (
-                questionCategory == null ||
-                answer == null ||
-                // If we care about a question already existing, want to avoid
-                // having 2 of the same question + answer combination
-                (careIfQuestionAlreadyExists &&
-                        quizQuestions.any {
-                            it.question == selector.questionDisplayName(questionCategory) && it.correctAnswer == answer
-                        })
-            ) {
-                failedTries++
-                continue
-            }
-
-            val otherOptions = allRelevantOtherOptionsCountries
-                .mapNotNull { optionCountry ->
-                    // Use the other options selector to get an option that's not the same as the answer
-                    selector.otherOptionsSelector(optionCountry)
-                        .takeIf { option ->
-                            // Check whether the country selected for the option would generate the same output
-                            // for the question category. If it does, we can't use it as an option. For example, if
-                            // the category is DrivingSide, the answers would never be the same, but the question category
-                            // for both the selected country and the option would be e.g. "Left"
-                            val doesOptionMatchQuestionCategory = (optionCountry as? QuestionAnswerType)?.let {
-                                selector.questionCategorySelector(it) == questionCategory
-                            } ?: false
-
-                            option != answer && !doesOptionMatchQuestionCategory
-                        }
-                }
-                .distinct()
-                .shuffled()
-                .take(numOfOptions - 1)
-
-            // If we couldn't get at least 1 other option, then skip this country as part of the question
-            if (otherOptions.isEmpty()) {
-                failedTries++
-                continue
-            }
-
-            quizQuestions.add(
-                QuizQuestion(
-                    question = selector.questionDisplayName(questionCategory),
-                    options = (otherOptions + answer).shuffled(),
-                    correctAnswer = answer
-                )
-            )
-
-            // Reset failed tries
-            failedTries = 0
-        }
-
-        return quizQuestions
-    }
 
     private fun generateQuizNew(
         numOfQuestions: Int,
--- src/main/kotlin/geotrainer/utils/quizselector/QuizTypeSelectors.kt
+++
@@ -1,3 +0,0 @@
-package geotrainer.utils.quizselector
-
-object QuizTypeSelectors
