740b28a722088a4f07422c1fb9766b680413f21f
Romans Kuznecovs <romansk@apadmi.com>
2023-12-08 00:03:02 +0000

    Clean up quiz routes and delete quiz use case

--- src/main/kotlin/geotrainer/feature/quiz/presentation/QuizRoutes.kt
+++ src/main/kotlin/geotrainer/feature/quiz/presentation/QuizRoutes.kt
@@ -39,21 +39,21 @@ fun Route.quizRouting() {
                 quizType = QuizType.valueOfOrNull(quizType) ?: return@get getInvalidError(quizTypeParam),
                 continent = Continent.valueOfOrNull(continent),
 
-                numOfQuestions = numOfQuestions.toIntOrNull()?.let { num ->
-                    if (num < 5 || num > 50) {
-                        return@get getInvalidError("$numOfQuestionsParam - must be 5 <= x <= 50")
+                numOfQuestions = numOfQuestions.toIntOrNull().let { num ->
+                    if (num == null || num < 5 || num > 50) {
+                        return@get getInvalidError("$numOfQuestionsParam - must be 5 <= x <= 50, and not null")
                     } else {
                         num
                     }
-                } ?: return@get getInvalidError(numOfQuestionsParam),
+                },
 
-                numOfOptions = numOfOptions.toIntOrNull()?.let { num ->
-                    if (num < 2 || num > 4) {
-                        return@get getInvalidError("$numOfOptionsParam - must be 2 <= x <= 4")
+                numOfOptions = numOfOptions.toIntOrNull().let { num ->
+                    if (num == null || num < 2 || num > 4) {
+                        return@get getInvalidError("$numOfOptionsParam - must be 2 <= x <= 4, and not null")
                     } else {
                         num
                     }
-                } ?: return@get getInvalidError(numOfOptionsParam)
+                }
             )
 
             call.respond(quizQuestions)
